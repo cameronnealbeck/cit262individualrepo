98146fbe360e2cd22418c1147939e8b16d876488
Cameron Beck <cameronnealbeck@gmail.com>
2022-06-02 10:52:41 -0600

    added url to index and login

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const application = express();
 
+application.use(bodyParser.json());
+
 application.get('/',(req,res)=>{res.send("Hello")});
+
+application.post('/login',(loginRequest,loginResponse)=>{
+    const userName = loginRequest.body.userName;
+    const password = loginResponse.body.password;
+
+    if (userName=="hatersgonnahate@byui.edu" && password=="P@ssw0rd"){
+        loginResponse.status(200);
+        loginResponse.send({Status:"LoggedIn"});
+    } else{
+        loginResponse.status(403);
+        loginResponse.send({Status:"FailedLoggingIn"});
+    }
+});
 
 application.listen(3000,()=>{console.log("Listening...")});
